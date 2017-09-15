16f7110f4f5ae0c086f6a439139190120033b7dd
Dan Smith <dev@dotherightthing.co.nz>
2017-09-15 15:44:56 +1200

    Get term children via generic function in wpdtrt framework;

--- app/wpdtrt-tourdates-getters.php
+++ app/wpdtrt-tourdates-getters.php
@@ -125,47 +125,6 @@ function wpdtrt_tourdates_get_post_daynumber($post_id) {
 }
 
 /**
- * Get the legs in a tour
- *  Used in wpdtrt-dbth/taxonomy-tours.php
- *
- * @link wpdtrt-dbth/taxonomy-tours.php
- * @param number $term_id The term ID
- * @param strong $order_function_name The name of a function which reorder the results
- * @return string $tour_leg_term_ids The tour leg IDs in order of start date
- *
- * @see https://www.advancedcustomfields.com/resources/get_field/
- * @see http://php.net/manual/en/function.is-callable.php
- * @see http://php.net/manual/en/function.call-user-func.php
- *
- * @todo Move into theme framework as wpdtrt_get_term_children_ordered
- */
-
-function wpdtrt_tourdates_get_tour_legs($term_id, $order_function_name=null) {
-
-  $tour_leg_term_ids = get_term_children( $term_id, get_query_var( 'taxonomy' ) );
-  $tour_leg_terms = array();
-
-  // build array containing term objects
-  foreach( $tour_leg_term_ids as $tour_leg_term_id ) {
-    $term = get_term_by( 'id', $tour_leg_term_id, get_query_var( 'taxonomy' ) );
-    $tour_leg_terms[] = $term;
-  }
-
-  if ( isset( $order_function_name ) && is_callable( $order_function_name ) ) {
-    $tour_leg_terms = call_user_func( $order_function_name, $tour_leg_terms );
-  }
-
-  // reset term ids array
-  $tour_leg_term_ids = array();
-
-  foreach( $tour_leg_terms as $tour_leg_term ) {
-    $tour_leg_term_ids[] = $tour_leg_term->term_id;
-  }
-
-  return $tour_leg_term_ids;
-}
-
-/**
  * Get the first date in a tour
  *
  * @param number $id The ID of the post OR term
