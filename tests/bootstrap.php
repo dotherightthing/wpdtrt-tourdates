e2788d7556faf50c5b86257329b663f206849ffb
Dan Smith <dev@dotherightthing.co.nz>
2017-12-15 12:12:22 +1300

    After updating WP-CLI from 1.1.0 to 1.4.1, regenerate WP-CLI scaffolding and helper paths;

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,12 +2,17 @@
 /**
  * PHPUnit bootstrap file
  *
- * @package wpdtrt_tourdates
+ * @package Wpdtrt_Tourdates
  */
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+
 if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Exception( "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
 }
 
 // Give access to tests_add_filter() function.
@@ -17,7 +22,6 @@ require_once $_tests_dir . '/includes/functions.php';
  * Manually load the plugin being tested.
  */
 function _manually_load_plugin() {
-	// TODO plugin file name is plugin specific
 	require dirname( dirname( __FILE__ ) ) . '/wpdtrt-tourdates.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
