c659e6234133e3feaf7408cee6dddbb089609706
Dan Smith <dev@dotherightthing.co.nz>
2017-12-19 12:24:33 +1300

    Fix config variable; Fixes #10;

--- js/frontend.js
+++ js/frontend.js
@@ -2,7 +2,7 @@
  * Scripts for the public front-end
  *
  * This file contains JavaScript.
- *    PHP variables are provided in wpdtrt_blocks_config.
+ *    PHP variables are provided in wpdtrt_tourdates_config.
  *
  * @package     wpdtrt_tourdates
  * @since       1.0.0
@@ -10,6 +10,6 @@
 
 jQuery(document).ready(function($){
 
-	var config = wpdtrt_blocks_config;
+	var config = wpdtrt_tourdates_config;
 
 })
