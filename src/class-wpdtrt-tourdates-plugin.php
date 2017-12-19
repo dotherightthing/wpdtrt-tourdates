d42a96ab3f49657d80c45bbfad76d8c467ae3aee
Dan Smith <dev@dotherightthing.co.nz>
2017-12-19 16:10:25 +1300

    Move CPT permalink rewriting into Plugin parent, as its purpose is to inject plugin variables;

--- src/class-wpdtrt-tourdates-plugin.php
+++ src/class-wpdtrt-tourdates-plugin.php
@@ -53,7 +53,6 @@ class WPDTRT_TourDates_Plugin extends DoTheRightThing\WPPlugin\Plugin {
 
     	parent::wp_setup();
 
-		add_action( 'post_type_link', 	[$this, 'render_permalink_placeholders'], 10, 3 ); // Custom Post Type
 		add_action( 'init', 			[$this, 'set_rewrite_rules'] );
 		add_action( 'save_post', 		[$this, 'save_post_daynumber'], 10, 3 );
 		add_filter( 'the_title', 		[$this, 'filter_post_title_add_day'] );
@@ -793,54 +792,6 @@ class WPDTRT_TourDates_Plugin extends DoTheRightThing\WPPlugin\Plugin {
 		return $the_link;
 	}
 
-	/**
-	 * Support Custom Field %placeholders% in Custom Post Type permalinks
-	 * 	This replacement is only applied when the permalink is generated
-	 * 	eg on an archive listing or wpadmin edit page
-	 *	NOT in the rewrite rules / when the page is loaded
-	 *
-	 * @param $permalink See WordPress function options
-	 * @param $post See WordPress function options
-	 * @param $leavename See WordPress function options
-	 * @return $permalink
-	 *
-	 * @example
-	 * 	// wpdtrt-dbth/library/register_post_type_tourdiaries.php
-	 * 	'rewrite' => array(
-	 * 		'slug' => 'tourdiaries/%tours%/%wpdtrt_tourdates_cf_daynumber%'
-	 * 		'with_front' => false
-	 * 	)
-	 *
-	 * @see http://shibashake.com/wordpress-theme/add-custom-taxonomy-tags-to-your-wordpress-permalinks
-	 * @see http://shibashake.com/wordpress-theme/custom-post-type-permalinks-part-2#conflict
-	 * @see https://stackoverflow.com/questions/7723457/wordpress-custom-type-permalink-containing-taxonomy-slug
-	 * @see https://kellenmace.com/edit-slug-button-missing-in-wordpress/
-	 * @see http://kb.dotherightthing.dan/php/wordpress/missing-permalink-edit-button/
-	 * @todo TourdatesTest\test_post
-	 */
-	public function render_permalink_placeholders($permalink, $post, $leavename) {
-
-		// Get post
-		$post_id = $post->ID;
-
-		// extract all %placeholders% from the permalink
-		// https://regex101.com/
-		preg_match_all('/(?<=\/%wpdtrt_tourdates_cf_).+?(?=%\/)/', $permalink, $placeholders, PREG_OFFSET_CAPTURE);
-
-		// placeholders in an array of taxonomy/term arrays
-		foreach ( $placeholders[0] as $placeholder ) {
-
-			$placeholder_name = 'wpdtrt_tourdates_cf_' . $placeholder[0];
-
-			if ( metadata_exists( 'post', $post_id, $placeholder_name ) ) {
-				$replacement = get_post_meta( $post_id, $placeholder_name, true );
-				$permalink = str_replace( ( '%' . $placeholder_name . '%' ), $replacement, $permalink);
-			}
-		}
-
-		return $permalink;
-	}
-
     //// END RENDERERS \\\\
 
     //// START FILTERS \\\\
@@ -1048,6 +999,7 @@ class WPDTRT_TourDates_Plugin extends DoTheRightThing\WPPlugin\Plugin {
 	     * @param $regex A regex to validate the value of the tag
 	     * @param $query Append query to queryreplace property array (optional)
 	     * @see https://codex.wordpress.org/Rewrite_API/add_rewrite_tag
+	     * @see $this->save_post_daynumber()
 	     */
 	    $wp_rewrite->add_rewrite_tag(
 	        '%wpdtrt_tourdates_cf_daynumber%',
