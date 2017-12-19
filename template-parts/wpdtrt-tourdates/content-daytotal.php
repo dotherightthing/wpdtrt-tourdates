03424be74a50e0a3f081dda21cf42ad8ff4b607e
Dan Smith <dev@dotherightthing.co.nz>
2017-12-19 17:24:02 +1300

    Fix wpdtrt_tourdates_shortcode_daynumber; Fix wpdtrt_tourdates_shortcode_daytotal;

--- template-parts/wpdtrt-tourdates/content-daytotal.php
+++ template-parts/wpdtrt-tourdates/content-daytotal.php
@@ -10,5 +10,37 @@
 ?>
 
 <?php
+	// Predeclare variables
+
+	// Internal WordPress arguments available to widgets
+	// This allows us to use the same template for shortcodes and front-end widgets
+	$before_widget = null; // register_sidebar
+	$before_title = null; // register_sidebar
+	$title = null;
+	$after_title = null; // register_sidebar
+	$after_widget = null; // register_sidebar
+
+	// shortcode options
+
+	// access to plugin
+	$plugin = null;
+
+	// Options: display $args + widget $instance settings + access to plugin
+	$options = get_query_var( 'options' );
+
+	// Overwrite variables from array values
+	// @link http://kb.network.dan/php/wordpress/extract/
+	extract( $options, EXTR_IF_EXISTS );
+
+	// WordPress widget options (widget, not shortcode)
+	echo $before_widget;
+	echo $before_title . $title . $after_title;
+
+	global $post;
+	$post_id = $post->ID;
+	$daytotal = $plugin->get_daytotal( $post_id, 'tour' );
 	echo $daytotal;
+
+	// output widget customisations (not output with shortcode)
+	echo $after_widget;
 ?>
