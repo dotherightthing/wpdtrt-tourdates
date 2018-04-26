0da1c0cae690bc183a0515438da5fc38930be110
Dan Smith <dev@dotherightthing.co.nz>
2018-04-27 09:31:49 +1200

    Add messages required by shortcode demo (dotherightthing/generator-wp-plugin-boilerplate#51)

--- wpdtrt-tourdates.php
+++ wpdtrt-tourdates.php
@@ -190,6 +190,8 @@ if( ! defined( 'WPDTRT_TOURDATES_URL' ) ) {
           'demo_shortcode_title' => __('Demo shortcode', 'wpdtrt-tourdates'),
           'demo_data_description' => __('This demo was generated from the following data', 'wpdtrt-tourdates'),
           'demo_date_last_updated' => __('Data last updated', 'wpdtrt-tourdates'),
+          'demo_data_length' => __('results', 'wpdtrt-tourdates'),
+          'demo_data_displayed_length' => __('results displayed', 'wpdtrt-tourdates'),
         ),
         'plugin_options' => $plugin_options,
         'instance_options' => $instance_options,
