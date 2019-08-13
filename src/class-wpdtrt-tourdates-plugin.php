743e3cf73ff4b6fcf7350299ad86f3ada13a2829
Dan Smith <dev@dotherightthing.co.nz>
2019-08-13 21:55:05 +1200

    Update wpdtrt-plugin-boilerplate from 1.5.5 to 1.5.6

--- src/class-wpdtrt-tourdates-plugin.php
+++ src/class-wpdtrt-tourdates-plugin.php
@@ -12,7 +12,7 @@
  *
  * @since   1.0.0
  */
-class WPDTRT_Tourdates_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_5\Plugin {
+class WPDTRT_Tourdates_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_5_6\Plugin {
 
 	/**
 	 * Supplement plugin initialisation.
