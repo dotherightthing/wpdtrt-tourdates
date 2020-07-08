c9624b0015485352641a91e42a0f5e6fc0784b28
Dan Smith <dev@dotherightthing.co.nz>
2020-07-08 23:44:04 +1200

    Lint PHP

--- wpdtrt-tourdates.php
+++ wpdtrt-tourdates.php
@@ -109,7 +109,7 @@ if ( is_admin() ) {
 	// This replaces the TGMPA autoloader
 	// @see dotherightthing/generator-wpdtrt-plugin-boilerplate#77
 	// @see dotherightthing/wpdtrt-plugin-boilerplate#136.
-	require_once( $project_root_path . 'vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php' );
+	require_once $project_root_path . 'vendor/tgmpa/tgm-plugin-activation/class-tgm-plugin-activation.php';
 }
 
 // sub classes, not loaded via PSR-4.
