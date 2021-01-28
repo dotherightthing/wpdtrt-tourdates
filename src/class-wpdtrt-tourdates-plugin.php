46627b7c4a2198792d0fa3d2ddbb77cb300dcd59
Dan Smith <dev@dotherightthing.co.nz>
2021-01-29 00:12:00 +1300

    Update wpdtrt-plugin-boilerplate from 1.7.13 to 1.7.14

--- src/class-wpdtrt-tourdates-plugin.php
+++ src/class-wpdtrt-tourdates-plugin.php
@@ -12,7 +12,7 @@
  *
  * @since   1.0.0
  */
-class WPDTRT_Tourdates_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_13\Plugin {
+class WPDTRT_Tourdates_Plugin extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_14\Plugin {
 
 	/**
 	 * Supplement plugin initialisation.
--- src/class-wpdtrt-tourdates-rewrite.php
+++ src/class-wpdtrt-tourdates-rewrite.php
@@ -14,7 +14,7 @@
  * @since   1.0.0
  * @version 1.0.0
  */
-class WPDTRT_Tourdates_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_13\Rewrite {
+class WPDTRT_Tourdates_Rewrite extends DoTheRightThing\WPDTRT_Plugin_Boilerplate\r_1_7_14\Rewrite {
 
 	/**
 	 * Supplement plugin initialisation.
