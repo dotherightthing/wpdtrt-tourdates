72d8d200a8ce82f7178c68e1b52e557d26bb1888
Dan Smith <dev@dotherightthing.co.nz>
2021-05-25 21:41:28 +1200

    Sync comment with plugin generator

--- wpdtrt-tourdates.php
+++ wpdtrt-tourdates.php
@@ -255,9 +255,12 @@ function wpdtrt_tourdates_plugin_init() {
 	);
 
 	/**
-	 * Demo shortcode
+	 * Array: demo_shortcode_params
 	 *
-	 * @see https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Settings-page:-Adding-a-demo-shortcode Settings page: Adding a demo shortcode
+	 * Demo shortcode.
+	 *
+	 * See:
+	 * - <Settings page - Adding a demo shortcode: https://github.com/dotherightthing/wpdtrt-plugin-boilerplate/wiki/Settings-page:-Adding-a-demo-shortcode>
 	 */
 
 	/**
